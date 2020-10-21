dcb714dc2d924a6fd28e2172b4750d48f918f849
Valentin-Marko <69463262+Valentin-Marko@users.noreply.github.com>
2020-10-21 12:07:15 +0300

    adding files

---
+++ smartbrain_back/controllers/signin.js
@@ -0,0 +1,31 @@
+const handleSignin = (req, res, db, bcrypt) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json("incorect form submision");
+  }
+  db.select("email", "hash")
+    .where("email", "=", email)
+    .from("login")
+    .then((data) => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      console.log(isValid);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", email)
+          .then((user) => {
+            console.log(user);
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json("unable to get user"));
+      } else {
+        res.status(400).json("wrong credentials");
+      }
+    })
+    .catch((err) => res.status(400).json("wrong credentials"));
+};
+
+module.exports = {
+  handleSignin: handleSignin,
+};
